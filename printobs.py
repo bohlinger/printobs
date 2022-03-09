f91f723a5f26357fca0680dd2e5879f2fa550aa0
bohlinger <patrikb@met.no>
2022-03-09 23:07:53 +0100

    exchanged multiple api calls with one call of multiple features

--- printobs.py
+++ printobs.py
@@ -2,6 +2,7 @@
 import argparse
 from argparse import RawTextHelpFormatter
 from datetime import datetime, timedelta
+from utils import parse_date
 
 parser = argparse.ArgumentParser(description="""
 print offshore insitu observations
@@ -9,44 +10,44 @@ print offshore insitu observations
 Usage:
 ./printobs.py
 """, formatter_class=RawTextHelpFormatter)
-parser.add_argument("-sd",
-                    metavar='startdate',
+parser.add_argument("-sd", metavar='startdate',
                     help="start date of time period to be downloaded")
-parser.add_argument("-ed",
-                    metavar='enddate',
+parser.add_argument("-ed", metavar='enddate',
                     help="end date of time period to be downloaded")
-parser.add_argument("-s",
-                    metavar='station',
-                    help="station")
-parser.add_argument("-i",
-                    metavar='instrument',
+parser.add_argument("-d", type=int, metavar='delta',
+                    help="substracted hours from now (in lieu of sd & ed)")
+parser.add_argument("-s", metavar='station', help="station")
+parser.add_argument("-i", metavar='instrument',
                     help="instrument")
 
 args = parser.parse_args()
+dargs = vars(args)
 
-if args.ed is None:
-    args.ed = datetime.now() + timedelta(hours=3)
+# remove all None entries
+dargs = {k: v for k, v in dargs.items() if v is not None}
 
-if args.sd is None:
-    args.sd = args.ed - timedelta(hours=6)
+ed = parse_date(dargs.get('ed',datetime.now() + timedelta(hours=3)))
+sd = parse_date(dargs.get('sd',datetime.now() - timedelta(hours=6)))
 
-if args.s is None:
-    args.s = 'valhall'
+if args.d is not None:
+    sd = parse_date(args.ed) - timedelta(hours=args.d)
 
-if args.i is None:
-    args.i = None
+s = dargs.get('s','valhall')
+i = None
+
+print('here')
 
 # -------------------------------------------------------------------- #
-from utils import get_all_ts, print_formatted
+from utils import get_frost_df, print_formatted
 
-varstr_lst = [
-        'air_temperature',
-        'wind_speed',
-        'wind_from_direction',
-        'sea_surface_wave_significant_height',
-        'sea_surface_wave_mean_period',
-        'sea_surface_wave_period_at_variance_spectral_density_maximum'
-        ]
+varstr_dict= {
+        'air_temperature':'   Ta',
+        'wind_speed':'   FF',
+        'wind_from_direction':'   DD',
+        'sea_surface_wave_significant_height':'   Hs',
+        'sea_surface_wave_mean_period':' Tm02',
+        'sea_surface_wave_period_at_variance_spectral_density_maximum':'   Tp'
+        }
 
-df = get_all_ts(args.sd,args.ed,args.s,varstr_lst)
-print_formatted(df,args.s)
+df = get_frost_df(sd,ed,s,varstr_dict)
+print_formatted(df,s)
--- utils.py
+++ utils.py
@@ -1,22 +1,25 @@
 from datetime import datetime
+import os
 from dateutil.parser import parse
 import pandas as pd
-import os
 import yaml
+import dotenv
+import requests
+
 
 with open('insitu_locations.yaml', 'r') as file:
     insitu_dict = yaml.safe_load(file)
 
 def parse_date(indate):
-    if isinstance(indate,datetime):
+    if isinstance(indate, datetime):
         return indate
-    elif isinstance(indate,str):
+    elif isinstance(indate, str):
         return parse(indate)
     else:
         print('Not able to parse input return as is')
         return indate
 
-def make_frost_reference_time_period(sdate,edate):
+def make_frost_reference_time_period(sdate, edate):
     sdate = parse_date(sdate)
     edate = parse_date(edate)
     formatstr = '%Y-%m-%dT%H:%M:00.000Z'
@@ -24,10 +27,9 @@ def make_frost_reference_time_period(sdate,edate):
                             edate.strftime(formatstr))
     return refstr
 
-def get_frost_ts(sdate,edate,nID,varstr):
-    import dotenv
-    import requests
-    import pandas as pd
+def get_frost_df(sdate,edate,nID,varstr_dict):
+    varstr_lst = list(varstr_dict.keys())
+    varstr = ','.join(varstr_lst)
     dotenv.load_dotenv()
     client_id = os.getenv('CLIENT_ID', None)
     if client_id is None:
@@ -40,25 +42,18 @@ def get_frost_ts(sdate,edate,nID,varstr):
                 'elements': varstr,
                 'referencetime': frost_reference_time,
                 }
-    r = requests.get(endpoint, parameters, auth=(client_id,client_id))
-    df = pd.json_normalize( r.json()['data'],
-                            ['observations'],
-                            ['sourceId','referenceTime'])
+    r = requests.get(endpoint, parameters, auth=(client_id, client_id))
+    d = {}
+    length = len(r.json()['data'])
+    d['referenceTime'] = [r.json()['data'][t]['referenceTime'] for t in range(length)]
+    for v,i in zip(varstr_lst,range(len(varstr_lst))):
+        d[v] = [r.json()['data'][t]['observations'][i]['value'] for t in range(length)]
+    df = pd.DataFrame(d)
+    varstr_dict['referenceTime'] = 'time'
+    df = df.rename(columns=varstr_dict)
     return df
 
-def get_all_ts(sdate,edate,nID,varstr_lst):
-    df = get_frost_ts(sdate,edate,nID,varstr_lst[0])['referenceTime']
-    for varstr in varstr_lst:
-        try:
-            dftmp = get_frost_ts(sdate,edate,nID,varstr)['value']
-            df = pd.concat([df, dftmp.reindex(df.index)], axis=1)
-        except Exception as e:
-            print(e)
-            print(varstr,' not available')
-            dftmp = pd.DataFrame([-999]*len(df))
-    return df
-
-def print_formatted(df,nID):
+def print_formatted(df, nID):
     print('\n'.join(df.to_string(index = False).split('\n')[1:]))
-    print('time                       TEMP     FF     DD     Hs   Tm02     Tp')
+    print('\n'.join(df.to_string(index = False).split('\n')[0:1]))
     print(nID)
