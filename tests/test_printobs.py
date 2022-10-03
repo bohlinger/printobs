f1310872d117ebe9ad61a70b8544c3c2b1775159
Patrik Bohlinger <patrikb@pc5591.pc.met.no>
2022-10-03 21:38:09 +0200

    added tests

--- tests/test_printobs.py
+++ tests/test_printobs.py
@@ -1,3 +1,14 @@
-from printobs import *
+import pytest
+from datetime import datetime
 
+def test_parse_date():
+    from printobs import parse_date
+    assert datetime(2022,1,1) == parse_date("2022-1-1")
 
+def test_call_frost_api():
+    from printobs import call_frost_api_v1
+    r = call_frost_api( sdate=datetime(2022,1,1),
+                        edate=datetime(2022,1,2),
+                        nID='draugen',v='v1')
+    print(vars(r.status_code).keys())
+    assert r.status_code == 200
